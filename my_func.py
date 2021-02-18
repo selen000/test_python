22ad7f0060b40454e9d65f98d320430c9d2548c2
ren <remremphobia@gmail.com>
2021-02-18 23:13:52 +0900

    可視化用

---
+++ my_func.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+def my_bar_plot(df, target_x, target_y):
+    """
+    棒グラフ可視化用関数
+    :param
+    df: 可視化対象のDataFrame
+    target_x : x軸のカラム名
+    target_y : y軸のカラム名
+    :return:
+    """
+    sns.barplot(x=target_x, y=target_y, data=df)
+    plt.show()
+
+def my_count_plot(df, target_x):
+    """
+    ヒストグラムを作成する
+    :param df:　集計対象のPandasDataFrame
+    :param target_x: 集計対象のカラム名
+    :return:
+    """
+    sns.countplot(x=target_x, data=df)
+    plt.show()
+
+def my_box_plot(df, target_x, target_y, target_hue=None):
+    """
+    箱ひげ図可視化
+    :param df:
+    :param target_x:
+    :param target_y:
+    target_hue : 区分値
+    :return:
+    """
+    ax = sns.boxplot(x=target_x, y=target_y, data=df, hue=target_hue)
+    plt.show()
+
+
+
+if __name__ == '__main__':
+
+    # テスト用のDataはタイタニックを使用
+    df = sns.load_dataset("titanic")
+
+    # 可視化テスト用
+    #my_bar_plot(df, target_x="class", target_y="age")
+    #my_count_plot(df,target_x="class")
+    my_box_plot(df, target_x="sex", target_y="age", target_hue="class")
+
+    
+
+
+
