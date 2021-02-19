b95bc10091c2b6ed93dbf23f9c8ab89384078024
ren <remremphobia@gmail.com>
2021-02-20 00:30:39 +0900

    設定情報yaml化

--- nlp.py
+++ nlp.py
@@ -3,10 +3,11 @@
 - つくりたいもの
  - 文章をしっかり管理できるツールを作成したい
 """
+import yaml
 
 from janome.tokenizer import Tokenizer
-tokenizer = Tokenizer()
 
+tokenizer = Tokenizer()
 
 def keitaiso(sentenses):
     """
@@ -21,17 +22,24 @@ def keitaiso(sentenses):
 
 
 if __name__ == __name__ == '__main__':
-    # Todo ファイル名称config化
-    file_name = "aaa.txt"
 
-    # Data読み込み
-    f = open('aaa.txt', 'r')
+    with open('setting.yaml', "r", encoding='utf8') as file:
+        config = yaml.safe_load(file.read())
+
+
+    # 設定情報取得
+    file_name = config["file_name"]
+    file_path = config["file_path"]
+
+    # 文章取得
+    f = open(file_name, 'r')
     data = f.read()
     f.close()
 
-    sentences = [data]
 
+    sentences = [data]
     # 形態素解析
     keitaiso(sentences)
+
 
 
