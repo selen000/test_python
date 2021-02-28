98bcd78b4f2e7987e1ca449ce0e904263408a42e
ren <remremphobia@gmail.com>
2021-03-01 01:50:34 +0900

    Add files via upload

--- my_pred.py
+++ my_pred.py
@@ -1,5 +1,7 @@
 # データを居れたら決定木で予測してくれるようなものを作りたい
 from sklearn import tree
+from sklearn.model_selection import train_test_split
+import pandas as pd
 import seaborn as sns
 
 
@@ -8,11 +10,27 @@ def _preprocess(df):
     前処理用関数
     :return:
     """
-    # 「train」の目的変数と説明変数の値を取得
-    target = df["survived"].values
-    features_one = df[["pclass", "sex", "age", "fare"]].values
+    df["age"] = df["age"].fillna(df["age"].median())
 
-    return target, features_one
+    df["sex"][df["sex"] == "male"] = 0
+    df["sex"][df["sex"] == "female"] = 1
+
+    #df["Embarked"][df["Embarked"] == "S"] = 0
+    #df["Embarked"][df["Embarked"] == "C"] = 1
+    #df["Embarked"][df["Embarked"] == "Q"] = 2
+    #df.Fare[152] = df.Fare.median()
+
+
+
+    # テストデータと訓練データに分割する
+    train_df, test_df = train_test_split(df, test_size=0.3)
+
+
+
+    # 「train」の目的変数と説明変数の値を取得
+    target = train_df["survived"].values
+    features_one = train_df[["pclass", "sex", "age", "fare"]].values
+    return target, features_one, train_df, test_df
 
 
 
@@ -25,9 +43,7 @@ if __name__ == '__main__':
     df = sns.load_dataset("titanic")
     print(df)
 
+    # 前処理
+    target, features_one, train_df, test_df = _preprocess(df)
 
-    target_df,features_one = _preprocess(df)
-
-    print(target_df)
-    print("-"*30)
-    print(features_one)
+    print(train_df)
