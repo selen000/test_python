b6209d687724ef2c4b395af4b6a184ed8eea1d3d
ren <remremphobia@gmail.com>
2021-03-04 22:53:24 +0900

    Add files via upload

---
+++ my_clustering.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+from sklearn.cluster import KMeans
+import my_plotting as m_plot
+
+def my_k_means(df, n):
+    """
+
+    :param df:
+    :return:
+    labels クラスタリング結果のラベル
+    """
+    kmeans_model = KMeans(n_clusters=n, random_state=10).fit(df)
+    # 分類結果のラベルを取得する
+    labels = kmeans_model.labels_
+    return kmeans_model, labels
+
+if __name__ == '__main__':
+    # テスト用のDataはタイタニックを使用
+    df = sns.load_dataset("iris")
+
+    # 学習
+    k_means_model, labels = my_k_means(df.iloc[:, 0:4], 3)
+
+    # ラベルを付与する
+    df["cluster"] = labels
+    target_x = df.columns[0]
+    target_y = df.columns[1]
+
+    # クラスタの分布
+    sns.countplot(x="cluster", data=df)
+
+    # クラスタごとに色分けされた散布図
+    m_plot.clusterring_scatter(df,target_x, target_y)
+
+
+
+
+
+
+
---
+++ my_plotting.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def func_box_plot(df, target_x, target_y):
+    """
+    箱ひげ図作成用関数
+    target_x : x軸カラム名
+    target_y : y軸カラム名
+    """
+    plt.figure(figsize=(10, 10))
+    ax = sns.boxplot(x=target_x, y=target_y, data=df)
+
+    ax.set_title("boxplot : {}_{}".format(target_x, target_y), fontsize=20)
+    ax.tick_params(labelsize=20)
+    plt.show()
+
+
+def func_swarm_plot(df,target_x, target_y):
+    """
+    Swarmplot作成用関数
+    target_x : x軸カラム名
+    target_y : y軸カラム名
+    """
+    plt.figure(figsize=(10, 5))
+    ax = sns.swarmplot(x=target_x, y=target_y, data=df)
+    ax.set_title("swarmplot : {}_{}".format(target_x,target_y), fontsize=20)
+    ax.tick_params(labelsize=20)
+    plt.show()
+
+
+def func_count_plot(df, target_x):
+    """
+    df : 棒グラフの集計
+    target_x : x軸カラム名
+    """
+    plt.figure(figsize=(10, 10))
+    ax = sns.countplot(x=target_x,data=df)
+    ax.set_title("countplot : {}".format(target_x), fontsize=20)
+    ax.tick_params(labelsize=20)
+    plt.show()
+
+
+def clusterring_scatter(df, target_x, target_y):
+    """
+    clusterカラムと、二次元の特徴量を持ったデータフレームをクラスタごとに、散布図
+    :param df:
+    :return:
+    """
+    plt.figure(figsize=(6, 6))
+    for cluster in df["cluster"].unique():
+        tmp_df = df[df["cluster"] == cluster]
+        plt.scatter(tmp_df[target_x], tmp_df[target_y], alpha=0.8)
+    plt.show()
