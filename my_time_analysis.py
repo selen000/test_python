dbf2e268e65ade1a784c499934bce1e1978e1ed4
ren <remremphobia@gmail.com>
2021-03-08 22:20:37 +0900

    Add files via upload

---
+++ my_time_analysis.py
@@ -0,0 +1,47 @@
+# 時系列分析用
+# とりあえずSARIMA実装
+
+import configparser
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+
+def sarima_learning(df):
+    """
+    # SARIMAモデル　学習用関数
+    :return:
+    """
+    return sm.tsa.SARIMAX(df, order=(3, 1, 2), seasonal_order=(1, 1, 1, 12)).fit()
+
+
+if __name__ == '__main__':
+
+    # 設定情報読み込み
+    ini = configparser.ConfigParser()
+    ini.read('./setup.ini', 'UTF-8')
+
+    #input_file_path = ini["FILE_PATH"]["CSV_FILE_PATH"]
+    #input_file_name = ini["FILE_PATH"]["FILE_NAME"]
+    #output_file_path = ini["FILE_PATH"]["OUTPUT_FILE_PATH"]
+    input_file_name = './data/AirPassengers.csv'
+
+    dateparse = lambda dates: pd.datetime.strptime(dates, '%Y-%m')
+    df = pd.read_csv(input_file_name, index_col='Month', date_parser=dateparse, dtype='float')
+
+    # 学習
+    SARIMA_3_1_2_111 = sarima_learning(df)
+
+    predict_start = "1960-01-01"
+    predict_end = "1965-12-01"
+
+    pred = SARIMA_3_1_2_111.predict('1960-01-01', '1965-12-01')
+
+    # 実データと予測結果の図示
+    plt.plot(df, label="actual")
+    plt.plot(pred, "r", label="predict")
+    plt.legend()
+    plt.show()
+
+
